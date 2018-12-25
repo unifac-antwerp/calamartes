d4d170699f2b01ed5070461d40baf4bc193a5e52
Wouter Landuydt <wouter.landuydt@gmail.com>
2018-12-25 22:25:29 +0100

    :heavy_plus_sign: added styled-components #3

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,11 +1,23 @@
 import { graphql, StaticQuery } from 'gatsby'
 import * as React from 'react'
-
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { normalize } from 'styled-normalize'
 import Header from './header'
 
 type TProps = {
   children: React.ReactNode
 }
+
+const theme = {
+  color: 'red',
+}
+const GlobalStyle = createGlobalStyle`
+  ${normalize}
+  
+  body {
+    background-color: yellow;
+  }
+`
 
 const Layout = ({ children }: TProps) => (
   <StaticQuery
@@ -19,19 +31,22 @@ const Layout = ({ children }: TProps) => (
       }
     `}
     render={data => (
-      <React.Fragment>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
-      </React.Fragment>
+      <ThemeProvider theme={theme}>
+        <React.Fragment>
+          <GlobalStyle />
+          <Header siteTitle={data.site.siteMetadata.title} />
+          <div
+            style={{
+              margin: `0 auto`,
+              maxWidth: 960,
+              padding: `0px 1.0875rem 1.45rem`,
+              paddingTop: 0,
+            }}
+          >
+            {children}
+          </div>
+        </React.Fragment>
+      </ThemeProvider>
     )}
   />
 )

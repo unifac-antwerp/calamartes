556bf3b3dd2b3c6285694680c239718169dea475
Wouter Landuydt <wouter.landuydt@gmail.com>
2018-12-26 13:47:04 +0100

    :chart_with_upwards_trend: disabled crawlers #6

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,14 @@
 /* tslint:disable:object-literal-sort-keys */
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = 'https://calamartes.netlify.com',
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV,
+} = process.env
+
+const isNetlifyProduction = NETLIFY_ENV === 'production'
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL
+
 module.exports = {
   siteMetadata: {
     title: `Calamartes`,
@@ -28,6 +38,30 @@ module.exports = {
         theme_color: `#ffffff`,
         display: `minimal-ui`,
         icon: `src/assets/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          production: {
+            // TODO: allow robots when site is ready
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
+          'branch-deploy': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
+          'deploy-preview': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
+        },
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality

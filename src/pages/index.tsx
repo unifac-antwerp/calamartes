0a769678c9f09fc72dc387c35d2a9a36980490ea
Wouter Landuydt <wouter.landuydt@gmail.com>
2018-12-26 15:37:11 +0100

    :card_file_box: test query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,28 @@
 import { Layout } from '@components'
+import { graphql, StaticQuery } from 'gatsby'
 import * as React from 'react'
 
 const IndexPage = () => (
-  <Layout>
-    <span>homepage</span>
-  </Layout>
+  <StaticQuery
+    query={graphql`
+      query allPrismicHomepageQuery {
+        allPrismicHomepage {
+          edges {
+            node {
+              data {
+                test
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Layout>
+        <span>{data.allPrismicHomepage.edges[0].node.data.test}</span>
+      </Layout>
+    )}
+  />
 )
 
 export default IndexPage

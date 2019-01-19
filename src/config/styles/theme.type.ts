7a54fdcb266a5c3f9ed793bbe8337748f4ebd58a
Wouter Landuydt <wouter.landuydt@gmail.com>
2019-01-19 15:30:10 +0100

    :label: added theme type

---
+++ src/config/styles/theme.type.ts
@@ -0,0 +1,58 @@
+import { CSSObject } from 'styled-components'
+
+export type TTheme = {
+  colors: TColors
+  typo: TTypo
+  grid: TGrid
+  breakpoints: TBreakpoints
+  shadows: TShadows
+}
+
+type TColors = {
+  neutral01: string
+  neutral02: string
+  global01: string
+}
+
+type TFonts = {
+  heading: string
+  default: string
+}
+
+type TFontweights = {
+  thin: number
+  extraLight: number
+  light: number
+  normal: number
+  medium: number
+  semiBold: number
+  bold: number
+  extraBold: number
+  black: number
+}
+
+type TTextstyle = {
+  heading01: CSSObject
+  paragraph: CSSObject
+}
+
+type TTypo = {
+  fonts: TFonts
+  fontweights: TFontweights
+  textstyle: TTextstyle
+}
+
+type TGrid = {
+  maxWidth: string
+}
+
+type TBreakpoints = {
+  s: string
+  m: string
+  l: string
+  xl: string
+}
+
+type TShadows = {
+  shadow01: CSSObject
+}
